c4fe42c9e0b26f85380bb3aefed83b93ecc32034
danodemano <danodemano@gmail.com>
2017-08-11 19:52:01 -0400

    Update uploadBI.ps1

--- uploadBI.ps1
+++ uploadBI.ps1
@@ -3,7 +3,7 @@
 # - Created by:			Dan Bunyard
 # - Email Address:		danodemano@gmail.com
 # - Creation date:		07/31/2017
-# - Modified date:		08/06/2017
+# - Modified date:		08/11/2017
 # - Filename:			uploadBI.ps1
 # - Description:			This script is designed to upload video recording from BlueIris to GoogleDrive
 # - All files are encrypted using the key provided in the below config before being uploaded.
@@ -38,7 +38,8 @@
 # - SOFTWARE.
 # - ************************************************************************************************
 # - Revisions:		1.00 - Initial release
-# - 				1.01 - Fixed SQL scripts to handle ' in filename
+# - 				1.01 - added check for lock and skip if file being written to
+# - 				
 # - ************************************************************************************************
 # - To do list:		[nothing]
 # - ************************************************************************************************
@@ -50,14 +51,14 @@
 #$filedir = ".\toupload" #The directory we are going to encrypt/upload
 $filedir = "C:\BlueIris\New"
 #$filedir = "Z:\"
-$tempdir = ".\temp" #The temp directory where we will store the encrypted files to upload
+$tempdir = "C:\Users\Administrator\Desktop\VideoUpload\temp" #The temp directory where we will store the encrypted files to upload
 #$tempdir = "C:\temp"
-$lockdir = ".\lock" #Where we store the lock file to prevent multiple instances running
+$lockdir = "C:\Users\Administrator\Desktop\VideoUpload\lock" #Where we store the lock file to prevent multiple instances running
 $recurse = $true #Set to $false to only upload files in the root of the above directory
 $retries = 3 #Number of times to retry the upload if it fails - after that we move on to the next file
-$gdriveconfigpath = ".\.gdrive" #assuming a .grive folder in the same directory of the script
-$encryption_key = "password" #randomly generate this!
-$upload_directory = "dir_id" #The ID of the directory we are uploading to
+$gdriveconfigpath = "C:\Users\Administrator\Desktop\VideoUpload\.gdrive" #assuming a .grive folder in the same directory of the script
+$encryption_key = "c0lL^r^Ri^8W%geL4X6oiDIx5MqzGm5m4%R1Z1YhDUrAhwn*LK" #randomly generate this!
+$upload_directory = "0B76apqWYV3oMYUFBTmY3bGJVUHc" #The ID of the directory we are uploading to
 
 # - ************************************************************************************************
 # - ************************************************************************************************
@@ -76,7 +77,26 @@ if (Test-Path "$lockdir\lock.txt") {
 	#Lock file already exists - exiting!
 	echo "Lock file exists - another copy is running.  Exiting!"
 	exit
-}
+} #END - if (Test-Path "$lockdir\lock.txt") {
+
+#This function is used to make sure the files isn't actively being written to
+function checkFileStatus($filePath)
+{
+	write-host (getDateTime) "[ACTION][FILECHECK] Checking if" $filePath "is locked"
+	$fileInfo = New-Object System.IO.FileInfo $filePath
+
+	try 
+	{
+		$fileStream = $fileInfo.Open( [System.IO.FileMode]::Open, [System.IO.FileAccess]::Read, [System.IO.FileShare]::Read )
+		write-host (getDateTime) "[ACTION][FILEAVAILABLE]" $filePath
+		return $true
+	}
+	catch
+	{
+		write-host (getDateTime) "[ACTION][FILELOCKED] $filePath is locked"
+		return $false
+	}
+} #END - function checkFileStatus($filePath)
 
 #Before continuing, create the lock file
 Add-Content "$lockdir\lock.txt" $pid
@@ -106,65 +126,70 @@ for ($i=0; $i -lt $files.Count; $i++) {
 	if ($gdrive_files -like "*$filename2*") { 
 		echo "File already exisist!  Moving to next." 
 	}else{
-		# - Get the size of the file
-		$filesize = (Get-Item "$filename").length
-		echo "File size: $filesize"
-		
-		# - Get the hash of the file we are about to encrypt for comparison
-		# - at a later date.  No worries about collisions as we will compare
-		# - both the hash and filename.  Also since we aren't using this
-		# - for anything authentication based it really doesn't matter.
-		$filehash = (Get-FileHash $filename -Algorithm "SHA1").Hash
-		echo "File SHA1 hash: $filehash"
-		
-		#Set the password to the encryption string
-		$password = $encryption_key
-		
-		echo "Encrypting file!"
-
-		# - The command we are going to use to encrypt the file
-		#echo ".\7zip\7za.exe a -t7z -mx0 -p"$password" "$tempdir\$filename2.7z" "$filename" -mhe"
-		$output = (.\7zip\7za.exe a -t7z -mx0 -p"$password" "$tempdir\$filename2.7z" "$filename" -mhe)
-		#exit
-
-		# - We should also get the filehash of the encrypted file as
-		# - yet another point of reference.
-		$encrypted_filehash = (Get-FileHash "$tempdir\$filename2.7z" -Algorithm "SHA1").Hash
-		echo "Encrypted file SHA1 hash: $encrypted_filehash"
-		
-		# - Get the size of the encrypted file
-		$encrypted_filesize = (Get-Item "$tempdir\$filename2.7z").length
-		echo "Encrypted file size: $encrypted_filesize"
-		
-		# - Another note letting the user know we are uploading the file
-		echo "Uploading file to Gdrive!"
-
-		# - Upload the file to Gdrive removing it when finished
-		$output = (.\gdrive\gdrive.exe --config "$gdriveconfigpath" upload --delete --parent "$upload_directory" "$tempdir\$filename2.7z")
-		
-		# - Need to make sure the file actually uploaded.
-		# - We will retry a number of times before giving up
-		# - and moving onto the next file.
-		$counter = 0
-		while ( ( $output -contains '*Failed*' ) -and ( $counter -lt $retries ) ) {
-			# - Upload the file to Gdrive removing it when finished
-			$output = (.\gdrive\gdrive.exe --config .\.gdrive upload --delete "$tempdir\$filename2.7z")
-			$counter++
-		} #END - while ( ( $output -contains '*Failed*' ) -and ( $counter -lt $retries ) ) {
-		
-		# - Check again if still failed
-		if ( $output -contains '*Failed*' ) {
-			# - Need to move on and NOT log this into the database
-			echo "Unable to complete upload, moving on"
-			
-			# - Cleanup the temp file so it doesn't linger
-			del "$tempdir\$filename2.7z"
+		#Ensure the files isn't locked and actively being written to
+		if (checkFileStatus($filename)) {
+			echo "File in use - skipping!"
 		}else{
-			echo "File uploaded!"
-		} #END - if ( $output -contains '*Failed*' ) {
-		
-		echo "Moving onto next file!"
-	}
+			# - Get the size of the file
+			$filesize = (Get-Item "$filename").length
+			echo "File size: $filesize"
+			
+			# - Get the hash of the file we are about to encrypt for comparison
+			# - at a later date.  No worries about collisions as we will compare
+			# - both the hash and filename.  Also since we aren't using this
+			# - for anything authentication based it really doesn't matter.
+			$filehash = (Get-FileHash $filename -Algorithm "SHA1").Hash
+			echo "File SHA1 hash: $filehash"
+			
+			#Set the password to the encryption string
+			$password = $encryption_key
+			
+			echo "Encrypting file!"
+
+			# - The command we are going to use to encrypt the file
+			#echo ".\7zip\7za.exe a -t7z -mx0 -p"$password" "$tempdir\$filename2.7z" "$filename" -mhe"
+			$output = (.\7zip\7za.exe a -t7z -mx0 -p"$password" "$tempdir\$filename2.7z" "$filename" -mhe)
+			#exit
+
+			# - We should also get the filehash of the encrypted file as
+			# - yet another point of reference.
+			$encrypted_filehash = (Get-FileHash "$tempdir\$filename2.7z" -Algorithm "SHA1").Hash
+			echo "Encrypted file SHA1 hash: $encrypted_filehash"
+			
+			# - Get the size of the encrypted file
+			$encrypted_filesize = (Get-Item "$tempdir\$filename2.7z").length
+			echo "Encrypted file size: $encrypted_filesize"
+			
+			# - Another note letting the user know we are uploading the file
+			echo "Uploading file to Gdrive!"
+
+			# - Upload the file to Gdrive removing it when finished
+			$output = (.\gdrive\gdrive.exe --config "$gdriveconfigpath" upload --delete --parent "$upload_directory" "$tempdir\$filename2.7z")
+			
+			# - Need to make sure the file actually uploaded.
+			# - We will retry a number of times before giving up
+			# - and moving onto the next file.
+			$counter = 0
+			while ( ( $output -contains '*Failed*' ) -and ( $counter -lt $retries ) ) {
+				# - Upload the file to Gdrive removing it when finished
+				$output = (.\gdrive\gdrive.exe --config .\.gdrive upload --delete "$tempdir\$filename2.7z")
+				$counter++
+			} #END - while ( ( $output -contains '*Failed*' ) -and ( $counter -lt $retries ) ) {
+			
+			# - Check again if still failed
+			if ( $output -contains '*Failed*' ) {
+				# - Need to move on and NOT log this into the database
+				echo "Unable to complete upload, moving on"
+				
+				# - Cleanup the temp file so it doesn't linger
+				del "$tempdir\$filename2.7z"
+			}else{
+				echo "File uploaded!"
+			} #END - if ( $output -contains '*Failed*' ) {
+			
+			echo "Moving onto next file!"
+		} #END - if (checkFileStatus($filename)) {
+	} #END - if ($gdrive_files -like "*$filename2*") { 
 } #END - for ($i=0; $i -lt $files.Count; $i++) {
 
 #Remove the lock file
